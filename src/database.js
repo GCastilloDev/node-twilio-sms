5647382525a0bba03a79da58704db028fa274b94
tavoGeek <gcnctz@gmail.com>
2020-05-06 00:38:10 -0500

    GCN

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(/*'mongodb://localhost/smsdb'*/
-process.env.DB, {
+process.env.BD, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
